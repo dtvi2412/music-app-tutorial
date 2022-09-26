a57beab8d0e6415e8ebfae1d4535a4ab02e94648
dtvi2412 <dtvi2412@gmail.com>
2022-09-26 22:15:03 +0700

    first push

--- src/App.js
+++ src/App.js
@@ -1,23 +1,109 @@
-import logo from './logo.svg';
+import { useState } from 'react';
+import AudioPlayer from 'react-h5-audio-player';
+import 'react-h5-audio-player/lib/styles.css';
 import './App.css';
-
+import { musics } from './data';
 function App() {
+  const [musicCurrent, setMusicCurrent] = useState({
+    id: null,
+    linkMusic: null,
+  });
+
+  const handlePlay = (id, linkMusic) => {
+    setMusicCurrent({
+      id: id,
+      linkMusic: linkMusic,
+    });
+  };
+
+  const handleNext = () => {
+    if (musicCurrent.id >= musics.length) {
+      setMusicCurrent({
+        id: 1,
+        linkMusic: musics[0],
+      });
+
+      return;
+    }
+
+    setMusicCurrent({
+      id: musicCurrent.id + 1,
+      linkMusic: musics[musicCurrent.id].linkMusic,
+    });
+  };
+
+  const handlePrev = () => {
+    if (musicCurrent.id === 1) {
+      setMusicCurrent({
+        id: musics.length,
+        linkMusic: musics[musics.length - 1].linkMusic,
+      });
+      return;
+    }
+
+    setMusicCurrent({
+      id: musicCurrent.id - 1,
+      linkMusic: musics[musicCurrent.id - 2].linkMusic,
+    });
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {musics.map((music) => (
+        <div key={music.id}>
+          <div
+            onClick={() => handlePlay(music.id, music.linkMusic)}
+            style={{
+              cursor: 'pointer',
+              display: 'flex',
+              alignItems: 'center',
+              backgroundColor: `${
+                music.id === musicCurrent.id ? '#3737ff' : '#000064'
+              }`,
+              padding: '14px',
+
+              border: `2px solid ${
+                music.id === musicCurrent.id ? '#96ff96' : '#00ba00'
+              }`,
+            }}
+          >
+            <img
+              style={{
+                width: 50,
+                height: 50,
+                borderRadius: '8px',
+                marginRight: 14,
+              }}
+              src={music.linkImg}
+              alt={music.name}
+            />
+            <h1 style={{ color: 'white', fontSize: '1rem' }}>{music.name}</h1>
+          </div>
+          {music.id === musicCurrent.id && (
+            <div
+              style={{
+                background: `url("https://wallpaper.dog/large/20496726.jpg")`,
+                width: '100%',
+                padding: '14px 0',
+              }}
+            >
+              <div className="circle">
+                <img
+                  src={music.linkImg}
+                  alt={music.name}
+                  style={{ width: 100, height: 100 }}
+                />
+              </div>
+              <AudioPlayer
+                onClickNext={handleNext}
+                onClickPrevious={handlePrev}
+                autoPlay
+                src={music.linkMusic}
+                showSkipControls={true}
+              />
+            </div>
+          )}
+        </div>
+      ))}
     </div>
   );
 }
---
+++ src/data.js
@@ -0,0 +1,83 @@
+import music1 from './assets/music/Johnning - WHAT THE HELL [NCS Release].mp3';
+import music2 from './assets/music/Sam Day & Dust of Apollon - Take You Home [NCS Release].mp3';
+import music3 from './assets/music/Invincible (feat. Joseph Feinstein) [NCS Release].mp3';
+import music4 from './assets/music/Culture Code - Make Me Move (feat. Karra) [NCS Release].mp3';
+import music5 from './assets/music/Egzod & Maestro Chives - Royalty (ft. Neoni) [NCS Release].mp3';
+import music6 from './assets/music/Elektronomia & JJD - Free [NCS Release].mp3';
+import music7 from './assets/music/Facading - Freefalling [NCS Release].mp3';
+import music8 from './assets/music/Lost Sky - Where We Started (feat. Jex) [NCS Release].mp3';
+import music9 from './assets/music/Robin Hustin x TobiMorrow - Light It Up (feat. Jex) [NCS Release].mp3';
+import music10 from './assets/music/Sub Urban - Cradles [NCS Release].mp3';
+import music11 from './assets/music/Tobu - Hope [NCS Release].mp3';
+
+export const musics = [
+  {
+    id: 1,
+    name: 'Johnning - WHAT THE HELL [NCS Release]',
+    linkMusic: music1,
+    linkImg: 'https://i.ytimg.com/vi/Mhp2W3O_cXU/mqdefault.jpg',
+  },
+  {
+    id: 2,
+    name: 'Sam Day & Dust of Apollon - Take You Home [NCS Release]',
+    linkMusic: music2,
+    linkImg: 'https://i.ytimg.com/vi/Ry54y9MFSiU/maxresdefault.jpg',
+  },
+  {
+    id: 3,
+    name: 'Invincible [NCS]',
+    linkMusic: music3,
+    linkImg: 'https://i1.sndcdn.com/artworks-000224592805-nhfd2x-t500x500.jpg',
+  },
+  {
+    id: 4,
+    name: 'Make Me Move (feat. Karra) [NCS Release]',
+    linkMusic: music4,
+    linkImg:
+      'https://ncsmusic.s3.eu-west-1.amazonaws.com/tracks/000/000/286/1000x0/make-me-move-feat-karra-1586948717-dTsA9So1cu.jpg',
+  },
+  {
+    id: 5,
+    name: 'Egzod & Maestro Chives - Royalty (ft. Neoni) [NCS Release]',
+    linkMusic: music5,
+    linkImg: 'https://i.ytimg.com/vi/OhX8GC3mm74/maxresdefault.jpg',
+  },
+  {
+    id: 6,
+    name: 'Elektronomia & JJD - Free [NCS Release]',
+    linkMusic: music6,
+    linkImg:
+      'https://ncsmusic.s3.eu-west-1.amazonaws.com/tracks/000/000/344/1000x0/free-1586950156-JOBrPMNa4n.jpg',
+  },
+  {
+    id: 7,
+    name: 'Facading - Freefalling [NCS Release]',
+    linkMusic: music7,
+    linkImg:
+      'https://i1.sndcdn.com/artworks-CjGHKgW3qzjlXcgy-WqWMUA-t500x500.jpg',
+  },
+  {
+    id: 8,
+    name: 'Lost Sky - Where We Started (feat. Jex) [NCS Release]',
+    linkMusic: music8,
+    linkImg: 'https://i.ytimg.com/vi/U9pGr6KMdyg/maxresdefault.jpg',
+  },
+  {
+    id: 9,
+    name: 'Robin Hustin x TobiMorrow - Light It Up (feat. Jex) [NCS Release]',
+    linkMusic: music9,
+    linkImg: 'https://i1.sndcdn.com/artworks-000387484215-dhnm65-t500x500.jpg',
+  },
+  {
+    id: 10,
+    name: 'Sub Urban - Cradles [NCS Release]',
+    linkMusic: music10,
+    linkImg: 'https://i.ytimg.com/vi/Hn4sfC2PbhI/maxresdefault.jpg',
+  },
+  {
+    id: 11,
+    name: 'Tobu - Hope [NCS Release]',
+    linkMusic: music11,
+    linkImg: 'https://i.ytimg.com/vi/EP625xQIGzs/maxresdefault.jpg',
+  },
+];
